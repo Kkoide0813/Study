f282499b93e9477498a5ebd580eff6e87d48437c
Kkoide0813 <qwejks0201@gmail.com>
2025-02-05 17:06:13 +0900

    System.out.println(fwd)

--- src/soccer/AbstractNormalPractice.java
+++ src/soccer/AbstractNormalPractice.java
@@ -4,24 +4,19 @@ public class AbstractNormalPractice {
     public static void main(String[] args) {
         // センターフォワードオブジェクトの生成
         SoccerPlayer fwd = new CenterForward("田村", 11);
-        // 自己紹介
+
+        // System.out.println()メソッドの裏側の処理がわからなかった。
+        // String valueOf(fwd)｛処理｝ → fwd.toString()が呼ばれる → fwd.toString(getPositionName() + "選手名" + this.name + "背番号" + this.uniformNumber))
         System.out.println(fwd);
+
         // ボールを処理
         fwd.kickBall();
         fwd.catchBall();
+
         System.out.println();
 
-        // ゴールキーパーオブジェクトの生成
-        SoccerPlayer keeper = new GoalKeeper("川島", 12);
-        // 自己紹介
-        System.out.println(keeper);
-        // ボールを処理
-        keeper.kickBall();
-        keeper.catchBall();
     }
 }
-
-
 // ここにサッカー選手抽象クラスを作成してください
 abstract class SoccerPlayer {
     private String name;
@@ -41,28 +36,25 @@ abstract class SoccerPlayer {
     public void catchBall(){
         System.out.println(this.name + "はボールを足で受け止めました");
     }
-//    // ポジション名取得 背番号はString型に変換が必要
-//    public String getPositionName(){
-//    }
+    // ポジション名取得 背番号はString型に変換が必要
 
+    public abstract String getPositionName();
+    @Override
+    public String toString(){
+        return getPositionName() + " 選手名 " + this.name + " 背番号 " + this.uniformNumber;
+    }
 }
 
 // 派生クラス・・・センターフォワード
 class CenterForward extends SoccerPlayer {
+
     // ポジション名 定数
-    final String POSITION_NAME = "センターフォワード";
+    public static final String POSITION_NAME = "センターフォワード";
 
     public CenterForward(String name, int uniformNumber) {
         super(name, uniformNumber);
     }
-}
-
-// 派生クラス・・・キーパー
-class GoalKeeper extends SoccerPlayer {
-    // ポジション名 定数
-    final String POSITION_NAME = "ゴールキーパー";
-
-    public GoalKeeper(String name, int uniformNumber) {
-        super(name, uniformNumber);
+    public String getPositionName() {
+        return POSITION_NAME;
     }
 }
